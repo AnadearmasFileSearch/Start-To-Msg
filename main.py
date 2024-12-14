614fc18340775a3c73dba124effac84ff30fa0dd
AnadearmasFileSearch <anad53951@gmail.com>
2024-12-14 19:14:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, fil
 from config import BOT_TOKEN
 from handlers import start, forward_message_to_admin, reply_to_user
 from handlers import broadcast, users, handle_user_blocked_or_deleted
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
@@ -16,7 +21,13 @@ def main():
     # Handle user blocking or deleting the bot
     app.add_handler(ChatMemberHandler(handle_user_blocked_or_deleted, ChatMemberHandler.MY_CHAT_MEMBER))
 
+    # Add an error handler
+    app.add_error_handler(error_handler)
+
     app.run_polling()
+
+async def error_handler(update, context):
+    logger.error(msg="Exception while handling an update:", exc_info=context.error)
 
 if __name__ == "__main__":
     main()

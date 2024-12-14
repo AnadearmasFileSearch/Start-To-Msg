abcba12f2ac300f018a6ab6ff9fb39f02689bdf9
AnadearmasFileSearch <anad53951@gmail.com>
2024-12-14 21:58:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ from config import BOT_TOKEN, ADMIN_ID, WEBHOOK_URL
 from handlers import start, forward_message_to_admin, reply_to_user, broadcast, users  # Import your handlers
 
 # Set up logging to get detailed information
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.DEBUG)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.DEBUG
+)
 logger = logging.getLogger(__name__)
 
 # Initialize the bot
@@ -21,7 +23,7 @@ application.add_handler(CommandHandler("start", start))
 application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, forward_message_to_admin))
 application.add_handler(CommandHandler("broadcast", broadcast))
 application.add_handler(CommandHandler("users", users))
-application.add_handler(MessageHandler(filters.TEXT & filters.Private, reply_to_user))
+application.add_handler(MessageHandler(filters.TEXT & filters.ChatType.PRIVATE, reply_to_user))
 
 # This function sets the webhook for Telegram updates
 def set_webhook():
